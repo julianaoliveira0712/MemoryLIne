5dd6fa2ad928372dde96db08050bf649c2b558e5
julianaoliveira0712 <julianasoliveira1998@gmail.com>
2019-09-01 14:02:48 -0300

    subindo

--- domain.py
+++ domain.py
@@ -1,9 +1,11 @@
 from resoucers import *
 import json
+import pymongo
 
 def allMemoryLine():
     cursor = conn.cursor()
     return json.dumps( cursor.execute('SELECT * from T_GAME;'))
     
+client = pymongo.MongoClient("mongodb+srv://remember_auth:hRmXnjYiziGTz3DP@cluster0-d2d9w.azure.mongodb.net/test?retryWrites=true&w=majority")
 
     
